3975d7bbbe8c06acf3cde3fc8bfc9c1e3221b6c4
Mitchell Long <meechllada@gmail.com>
2017-08-18 10:12:28 -0700

    format output to populate an unordered List

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,20 @@
 //Business Logic
 var pong = function (number){
-
-  var ping =[];
+  var result =[];
 
   for(var i = 1; i<=number; i++){
-    if(i%3 ===0){
-      ping.push("ping")
+    if(i%3 ===0 && i %5 ===0){
+      result.push("Ping-pong")
+    }else if (i %3 ===0) {
+      result.push("ping");
     }else if (i %5 ===0) {
-      ping.push("pong");
+      result.push("pong");
     }else{
-      ping.push(i);
-    }
+      result.push(i);
+    };
   };
-  return ping;
+  return result;
 };
-
-
 
 
 
@@ -27,6 +26,15 @@ $(document).ready(function(){
     event.preventDefault();
     var usersNumber = parseInt($("input#number").val());
     console.log(usersNumber);
-    var result = $("#result").text(pong(usersNumber));
+    var pingPongArray = pong(usersNumber);
+    // var bullets = $("#bullets").text(resultsBullets(pingPongArray));
+    console.log("pingPongArray:"+pingPongArray.length + ":"+ "userNumber:"+usersNumber.length)
+
+    var elements = [];
+    for(x = 0; x < pingPongArray.length; x++) {
+        var element = $('<li>'+pingPongArray[x]+'</li>');
+        elements.push(element);
+    }
+    $('#bullets').append(elements);
   });
 });
